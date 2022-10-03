1a8bea49d9075eb52ab4985a7c60a79ae01f2b00
Sarath6369 <77319100+Sarath6369@users.noreply.github.com>
2022-10-03 20:38:39 +0530

    Add files via upload

---
+++ SwiftScriptDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftScriptDemo
+//
+//  Created by Sarath D on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
